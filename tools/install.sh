98b19f60b0d146ba828f3155b33f6e76508deb9f
Sumanta Bhui <sumanta.bhui@bidchat.com>
2020-05-07 08:41:15 +0530

    Can't use helper in install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -8,7 +8,32 @@ REPO=${REPO:-sumantabidchat/grassdoor-cli-poc}
 REMOTE=${REMOTE:-https://github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
-source ../helper.sh
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
+
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+error() {
+	echo ${RED}"Error: $@"${RESET} >&2
+}
 
 clone_project() {
 	# Prevent the cloned repository from having insecure permissions. Failing to do
